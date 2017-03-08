a207288c0e3ba96f503bede9bc0293add0f93841
Younghun-Jung <youngj6169@gmail.com>
2017-03-08 10:45:10 +0900

    update loader in module section

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         loaders: [
             {
                 test: /.js$/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,

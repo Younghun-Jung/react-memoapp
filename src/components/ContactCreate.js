bc8620e71d482c847631e32721bf33ff41caf5d2
Younghun-Jung <youngj6169@gmail.com>
2017-03-10 02:18:12 +0900

    Add component for create contact data

---
+++ src/components/ContactCreate.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ContactCreate extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Create Contact</h2>
+                <p>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="name"
+                    />
+                    <input
+                        type="text"
+                        name="phone"
+                        placeholder="phone"
+                    />
+                </p>
+                <button>Create</button>
+            </div>
+        )
+    }
+}

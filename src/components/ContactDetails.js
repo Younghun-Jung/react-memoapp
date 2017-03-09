3b51d5dc79f3daae272309ab8ec3c3c70784a8ad
Younghun-Jung <youngj6169@gmail.com>
2017-03-09 22:39:56 +0900

    Check select name or not

---
+++ src/components/ContactDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class ContactDetails extends React.Component {
+    render() {
+        const details = (<div>Selected</div>);
+        const blank = (<div>Not Selected</div>);
+
+        return (
+            <div>
+                {this.props.isSelected ? details : blank}
+            </div>
+        );
+    }
+}

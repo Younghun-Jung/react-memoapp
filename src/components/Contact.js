7d7519e7a1ce2d24620cfc09d48251c6621a873d
Younghun-Jung <youngj6169@gmail.com>
2017-03-12 19:44:41 +0900

    Add onCreate props on ContactDetails compo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -58,6 +58,9 @@ export default class Contact extends React.Component {
         });
     }
     handleRemove() { // selectedKey를 사용해서 삭제
+        if(this.state.selectedKey < 0) { // 예외처리: 선택되지 않은 경우 종료
+            return;
+        }
         this.setState({
             contactData: update(this.state.contactData,
                 { $splice: [[this.state.selectedKey, 1]] }
@@ -94,7 +97,7 @@ export default class Contact extends React.Component {
                     <ContactInfo
                         contact={contact}
                         key={i}
-                        /* 컴포넌트에는 적용안되며 해당 컴포넌트의 props로 넘겨서 사용. 바로 사용하는 것이 아니라 arrow function 사용해서 클릭되면 실행되도록 */
+                        // 컴포넌트에는 적용안되며 해당 컴포넌트의 props로 넘겨서 사용. 바로 사용하는 것이 아니라 arrow function 사용해서 클릭되면 실행되도록
                         onClick={()=>this.handleClick(i)}
                     />
                 );
@@ -112,11 +115,12 @@ export default class Contact extends React.Component {
                 />
                 <div>{mapToComponent(this.state.contactData)}</div>
                 <ContactDetails
-                    isSelected={this.state.selectedKey != -1} /* 선택되면 true 전달 */
-                    contact={this.state.contactData[this.state.selectedKey]} /* 선택된 인덱스에 해당하는 주소 반영 */
+                    isSelected={this.state.selectedKey != -1} // 선택되면 true 전달
+                    contact={this.state.contactData[this.state.selectedKey]} // 선택된 인덱스에 해당하는 주소 반영
+                    onRemove={this.handleRemove} // ContactDetails 컴포넌트에 handleRemove를 props로 전달
                 />
                 <ContactCreate
-                    onCreate = {this.handleClick} /*ContactCreate 컴포넌트에 handleClick를 props로 전달*/
+                    onCreate = {this.handleClick} // ContactCreate 컴포넌트에 handleClick를 props로 전달
                 />
             </div>
         );
